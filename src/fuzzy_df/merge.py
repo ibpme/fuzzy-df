4c2843b19e9e2b990158d812b3d96351ba50a692
Iman-Budi Pranakasih <ibpranakasih@gmail.com>
2025-03-26 12:44:36 +0700

    chore: generate merge docs + add how inner exception

--- src/fuzzy_df/merge.py
+++ src/fuzzy_df/merge.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from pandas._typing import MergeHow, IndexLabel, AnyArrayLike
-from .match import fuzz_match
+from pandas._typing import AnyArrayLike, IndexLabel, MergeHow
+
+from fuzzy_df.match import fuzz_match
 
 
 def fuzz_merge(
@@ -36,12 +37,29 @@ def fuzz_merge(
         ValueError: If `on`, `left_on`, or `right_on` are not provided.
         NotImplementedError: If multiple columns are specified for `left_on` or `right_on`.
 
+    Example:
+        >>> left = pd.DataFrame(
+            {"id_left": [1, 2], "name_left": ["foo", "bar"]})
+        >>> right = pd.DataFrame(
+            {"id_right": [3, 4, 5, 6], "name_right": ["baz", "bear", "fool", "food"]})
+        >>> merged = fuzz_merge(left, right, left_on="name_left",
+                    right_on="name_right", score_cutoff=70)
+    ```
+        id_right name_right  id_left name_left  left_index  right_index      score
+    2         4       bear        2       bar           1            1  85.714287
+    0         5       fool        1       foo           0            2  85.714287
+    1         6       food        1       foo           0            3  85.714287
+    ```
+
     Notes:
         - The function uses fuzzy matching to compare the specified columns in the left and right DataFrames.
         - The `fuzz_match` function is expected to handle the actual fuzzy matching logic.
         - The resulting DataFrame includes the similarity score and merges the left and right DataFrames based on 
           the specified `how` parameter.
     """
+    if how != 'inner':
+        raise NotImplementedError(
+            "Non inner join is supported not supported yet")
     left_on = left_on if left_on is not None else on
     right_on = right_on if right_on is not None else on
     if left_on is None or right_on is None:
@@ -57,7 +75,7 @@ def fuzz_merge(
     matched_df = fuzz_match(comp_left, comp_right, score_col, score_cutoff)
 
     matched_df = pd.merge(
-        left.reset_index(), matched_df, left_index=True, right_on='left_index', how=how, validate="one_to_one")
+        left.reset_index(drop=True), matched_df, left_index=True, right_on='left_index', how=how)
     matched_df = pd.merge(
-        right.reset_index(), matched_df, left_index=True, right_on='right_index', how=how, validate="one_to_one")
+        right.reset_index(drop=True), matched_df, left_index=True, right_on='right_index', how=how)
     return matched_df
